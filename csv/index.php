cee94a903848140ba54050c00caf25df51142373
nosvalds <niko@devinit.org>
2023-01-03 10:10:16 +0000

    sync from live before removal of VPS server

---
+++ csv/index.php
@@ -0,0 +1,241 @@
+<?php
+include("../config/config.php");
+	
+function count_results_file($file) {
+	if(is_int($_GET['total'])) { 
+	 	return $_GET['total'];
+	} else {
+		$data = file_get_contents(EXIST_URI.EXIST_DB."/".$file."?_query=count(//iati-activity)");
+		return trim(strip_tags($data));
+	}
+}
+
+function last_updated($file) {
+
+	$xml = simplexml_load_file(EXIST_URI.EXIST_DB);
+	$xml->registerXPathNamespace('exist', 'http://exist.sourceforge.net/NS/exist');
+	$last_updated = $xml->xpath("//exist:resource[@name='".$file."']");
+
+	foreach($last_updated as $node) {
+		foreach($node->attributes() as $att => $value) { 
+		    if ($att == "last-modified") { 
+				return $value;
+		    } 
+		  }
+	}
+		return null;
+}
+
+//Hard coded defaults at the moment. 
+function fetch_xsl($xsl=null,$elementName = "") {
+	if(!$xsl) {
+		$xsl = array("Activities CSV"=>"/db/xsl/iati-activities-xml-to-csv.xsl",
+	"Transactions CSV"=>"/db/xsl/iati-transactions-xml-to-csv.xsl");
+	}
+	
+	$return .="<select name=\"$elementName\" class=\"change_xsl\" id=\"{$elementName}_select\">";
+	foreach($xsl as $name => $stylesheet) {
+		$return .= "<option value=\"".str_replace(" ","_",strtolower($name))."\">$name</option>";
+	}
+	$return .= "</select>";
+	
+	$return .= "<div class=\"xsl_list\" id=\"{$elementName}_list\">";
+	foreach($xsl as $name => $stylesheet) {
+		$return .= "<span id=\"".str_replace(" ","_",strtolower($name))."\">$stylesheet</span>";
+	}
+	$return .= "</div>";
+	
+	return $return;
+}
+$default_xsl = "https://raw.github.com/aidinfolabs/IATI-XSLT/master/templates/csv/iati-activities-xml-to-csv.xsl";
+
+function load_datasets() {
+    $xml = simplexml_load_file(EXIST_URI.EXIST_DB.""); 
+	foreach($xml->xpath("//exist:resource") as $resource) {
+		$return[] = (string)$resource->attributes()->name;
+	}
+	asort($return);
+	return $return;
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>IATI Explorer Toolkit - CSV Tools</title>
+	<link rel="stylesheet" type="text/css" media="all" href="/css/style.css" />
+	<!--[if IE]>
+	<link rel="stylesheet" type="text/css" media="all" href="/css/style-ie.css" />
+	<![endif]-->
+	<link rel="stylesheet" type="text/css" media="all" href="/css/custom.css" />
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script> 
+	<script type="text/javascript" src="/js/iati-query-builder.js"></script> 
+	<script type="text/javascript" src="/js/jquery.unescape.js"></script> 
+	<link rel="stylesheet" type="text/css" media="all" href="/css/iati-toolkit.css" />	
+	<style><!--	
+		.xsl_list {
+			display:none;
+		}
+		
+	--></style>	
+</head>
+<body>
+	<div id="wrapper" class="hfeed">
+		<div id="header">
+			<div id="masthead" class="clearfix">
+				<div id="branding" role="banner">
+	            	<img src="/images/iati-toolkit-logo-draft.png" border="0" />
+	                <div id="site-description">a collection of tools for working with IATI Data</div>
+	            </div><!-- #branding -->
+
+
+				<div id="access" class="access clearfix" role="navigation">
+				  				<div class="skip-link screen-reader-text"><a href="#content" title="Skip to content">Skip to content</a></div>
+									<div class="menu"><?php include("../includes/menu.inc.php"); ?></div>			</div><!-- #access -->
+			</div><!-- #masthead -->
+		</div><!-- #header -->
+	
+	
+		<div id="main" class="clearfix">
+
+			<div id="container">
+				<div id="content" role="main">
+					<div class="post type-post status-publish format-standard">
+						<h2 class="entry-title">View IATI Donor Files as CSV</h2>
+						
+							<div class="intro">
+							<p>The authoritative version of any IATI data is always the XML direct from the donor. This service will apply your chosen approach to 'flatten' out the data into CSV form to open and explore it in a spreadsheet based on a cached copy of the data. This system checks for new data daily, and will fetch an updated copy of donors data whenever it detects a change.
+							</p>
+							<p>
+								If you want a more flexible system to generate CSV from across different IATI files (e.g. by country, regardless of donor), please use the <a href="/query/">Query builder</a>.
+							</p>
+							</div>
+							
+<?php
+if($file = $_GET["file"]) { 
+	$total = count_results_file($file);
+	$howmany = $_GET['howmany'] ? $_GET['howmany'] : 1000;
+	if($howmany > 10000) { $howmany = 1000; }
+
+	if($updated = last_updated($file)) {
+	
+		echo "<p>Our records for ".$file." were last updated on ". date("D M Y H:m",strtotime($updated))." </p>";
+		
+		echo "<div class='result-count'>This file contains ".$total." activities. It may contain more or less transactions.</div>";
+		
+		echo "<div class='select-format'>".fetch_xsl(null,"csvxsl")."</div>"; 
+
+	
+	} else {
+		echo "<p>We do not have a record for the file you have requested. It is possible it has not yet been fetched, or is no longer available from the <a href=\"http://www.iatiregistry.org\">IATI Registry</a>. This service may be up to 24 hours behind the IATI Registry. ";
+	
+	}
+
+
+
+
+	if($total < $howmany) { 
+		$url = "fetch.php?file=".$_GET['file']."&howmany=$howmany&page={$pn}&xsl=".($_GET['xsl'] ? $_GET['xsl'] : $default_xsl)."";
+		echo "<p>You can fetch your data in a single file. Click the link below to download.</p>";
+		echo "<p><a href=\"$url\" class=\"link_with_xsl\">Download $total entries</a></p>";
+	} else {
+		$pages = round($total / $howmany,0,PHP_ROUND_HALF_UP);
+		echo "<div class='result-pages'>";
+		echo "<p>To avoid large files and queries causing difficulty we'll need to fetch this in $pages separate sections. Select from the links below on turn to fetch your data. You can then recombine this data in your local software</p>";
+	
+		for($pn = 1;$pn <= $pages;$pn++) {
+			$page_array[] = "<a href=\"fetch.php?file=".$_GET['file']."&howmany=$howmany&page={$pn}&xsl=".($_GET['xsl'] ? $_GET['xsl'] : $default_xsl)."\" class=\"link_with_xsl\">Page $pn</a>";
+		}
+		echo "<div class='page-list'>".join($page_array,", ")."</div>";
+		echo "</div>";							
+	}?>
+
+									<p></p>
+									<div class="note">
+										<h3>Notes</h3>
+										<p>
+										The data you are accessing may have been transformed and reformatted using third-party code. Make sure you understand any limitations of your chosen conversion before relying on the data.
+										</p>
+										<h3>Fetching direct</h3>
+										<p>
+											If you want to fetch this data direct and page through results in your own applications, you can use the following URL. Use the '_start' and '_howmany' parameters for paging, and the _xsl parameter to apply a style sheet to the results. Stylesheets can be loaded over the Internet, although output will be XML and your application will need to handle any content type selection required. Check the exist:hits and exist:count values in XML output to see how far you need to page through the results. 
+										</p>
+										<p>
+											<input type="text" size="60" value="<?php echo EXIST_URI.EXIST_DB."?_query=".$_GET['query'].($_GET["xsl"] ? "&_xsl=".$_GET["xsl"] : null); ?>">
+										</p>
+									</div>	
+									
+									<?php
+								} else {?>
+									<h3>Select your file</h3>
+									<div>
+									This CSV service expects a package ID in the ?file= parameter. E.g. ?file=dfid-af for DFID's Afghanistan data.
+									</div>
+									<div>
+										<form action="." method="get">
+										<strong>Currently available files:</strong>  
+										<select name="file">
+											<?php foreach(load_datasets() as $dataset) {
+												echo "<option value='$dataset'>$dataset</option>";
+											}?>
+										</select>
+										<input type="submit" value="Fetch"/>
+									</div>
+									
+									
+							<?php	}
+							?>
+
+						
+					</div><!--.post-->
+					
+				</div><!-- #content -->
+			</div><!-- #container -->
+
+
+
+			<div id="sidebar" class="widget-area" role="complementary">
+				<ul class="xoxo">
+					<li id="text-3" class="widget-container widget_text"><h3 class="widget-title">Using Query Builder</h3>
+						<div class="widget-content">
+							<p>
+							Data is published part of the International Aid Transparency Initiative by individual donors, often in a different file for each country they work in, and is listed in the <a href="http://iatiregistry.org/">IATI Registry</a>. This tool uses data fetched from all those files, and aggregated together in an <a href="http://www.exist-db.org">XML database</a> to allow you to query the data and convert it into the format you want.
+							</p>
+							<p>
+							Use the query builder to choose the sub-set of data you are interested in, and then choose the way you watch to explore it from the options below.
+							</p>							
+						</div>
+					</li>
+				</ul>
+
+			</div><!-- #sidebar .widget-area -->
+		</div><!-- #main -->
+
+				<div id="footer" role="contentinfo"> 
+					<div id="colophon"> 
+ 						<div id="site-generator" class="clearfix"> 
+						</div><!-- #site-generator --> 
+					</div><!-- #colophon --> 
+				</div><!-- #footer --> 
+
+			</div><!-- #wrapper -->
+	
+	
+
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45255890-1', 'aidinfolabs.org');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,300 +1,116 @@
 <?php
-// Hide errors
-error_reporting(0);
-ini_set('display_errors', 0);
-
-// Override default PHP configurations
-ini_set('max_execution_time',360);
-ini_set('max_memory','512M');
-
-  $transformations = array(
-  	"simple" => array("xslt" => "csv/simple-activity-listing.xsl",
-					"title" => "Simple activity summary",
-					"description" => "Top-level information with a row for each aid activity. Total figures for each type of transaction. Sector codes are included as a ';' separated list. Useful for basic research.",
-					"notes" => "If an activity contains transactions in multiple currencies this will not be taken into account when transaction totals are calculated. To ensure accuracy in financial analysis use the transaction data."),
-	"full" => array("xslt" => "csv/iati-activities-xml-to-csv.xsl",
-					"title" => "Full activity data",
-					"description" => "Full flattened version of each IATI Activity record - one row per activity. Where multiple values exist for a column (e.g. multiple sectors, or many transactions) these are separated by ';'.",
-					"notes" => "This data can be reshaped using a tool like Google Refine for analysis along any of it's dimensions. If an activity contains transactions in multiple currencies this will not be taken into account when transaction totals are calculated. To ensure accuracy in financial analysis use the transaction data. "),
-	"transac" => array("xslt" => "csv/iati-transactions-xml-to-csv.xsl",
-						"title" => "All transactions",
-						"description" => "Detailed list of all transactions, with currencies, amounts and classifications with a row for each transaction.",
-						"notes" => "")
-  );
-
- if($_GET['download']) {
-	header("Cache-Control: public");
-	header("Content-Description: File Transfer");
-	header("Content-Disposition: attachment; filename=iati_download_".date("Ymd")."_".$_GET['format']."_".$_GET['id'].".csv");
-	header("Content-type: text/csv");
-	$xml = urldecode($_GET['xml']);
-	$xmlDoc = new DOMDocument;
-	$data = file_get_contents(str_replace(' ','%20',$xml));
-	$xmlDoc->loadXML($data);
-
-	if($xmlDoc->getElementsByTagName('iati-activity')->item(0)) {
-	   $xslDoc = new DOMDocument();
-   	   $xslDoc->load($transformations[$_GET['format']]['xslt']);
-
-	   $proc = new XSLTProcessor();
-	   $proc->importStylesheet($xslDoc);
-
-	   $outdir = "";
-
-	   $proc->transformToURI($xmlDoc, 'php://output');
-	   ob_flush();
-	   exit();
-
-   } else {
-       $complete = 1;
-	   $error = "We could not find IATI data for the country or option you selected";
-   }
-}
-
+include("config/config.php");
 ?>
-<html>
-	<head>
-		<title>Raw data preview and tools</title>
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-		<style><!--
-		* {
-	        margin: 0;
-	        padding: 0;
-	    }
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
 
-		p {
-			margin-bottom:1em;
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>IATI Explorer Toolkit</title>
+	<link rel="stylesheet" type="text/css" media="all" href="/css/style.css" />
+	<!--[if IE]>
+	<link rel="stylesheet" type="text/css" media="all" href="/css/style-ie.css" />
+	<![endif]-->
+	<link rel="stylesheet" type="text/css" media="all" href="/css/custom.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="/css/iati-toolkit.css" />	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script> 	
+	<script type="text/javascript" src="/js/display.js"></script> 
 
-		}
-	    body {
-	        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-	        margin: 10px;
-	        color: #292416;
-	        background-color: #f8f8f8;
-	    }
+</head>
+<body>
+	<div id="wrapper" class="hfeed">
+		<div id="header">
+			<div id="masthead" class="clearfix">
+				<div id="branding" role="banner">
+	            	<img src="/images/iati-toolkit-logo-draft.png" border="0" />
+	                <div id="site-description">a collection of tools for working with IATI Data</div>
+	            </div><!-- #branding -->
 
-	    h1 {
-	        font-size: 24px;
-	        border-bottom: 1px solid #b8ad98;
-	        padding: 0 5px 3px 5px;
-	        margin: 0 10px 5px 10px;
-	    }
+				<div id="access" class="access clearfix" role="navigation">
+				  				<div class="skip-link screen-reader-text"><a href="#content" title="Skip to content">Skip to content</a></div>
+									<div class="menu"><?php include("includes/menu.inc.php"); ?></div>			</div><!-- #access -->
+			</div><!-- #masthead -->
+		</div><!-- #header -->
+	
+	
+		<div id="main" class="clearfix">
+
+			<div id="container">
+				<div id="content" role="main">
+					<div class="post type-post status-publish format-standard">
+						<h2 class="entry-title">IATI Explorer Toolkit</h2>
+						
+						<p>
+							The International Aid Transparency Initiative has created an XML standard and processes for donors to publish information on aid projects, budgets and spending. A log of all published IATI data is kept on the <a href="http://www.iatiregistry.org">IATI Registry</a> where you can download XML files directly from donors and other information sources. 
+						</p>
+						
+						<p>
+							During 2011 this site hosted a number of experimental tools for accessing IATI data. A number of these are now deployed directly on the <a 
+href="http://www.iatiregistry.org">IATI Registry</a> or on third-party platforms like the <a href="http://www.iatiexplorer.org">IATI Explorer</a>.
+						</p>
+						<p>
+							You can find a list of tools for working with IATI Data on the <a href="http://wiki.iatistandard.org">IATI Technical Community Wiki</a>. Further tools to provide end-user 
+access to IATI data are currently under development.
+						</p>						
+					</div><!--.post-->
+					
+				</div><!-- #content -->
+			</div><!-- #container -->
 
 
-	    div {
-	        background-color: #fff;
-	        padding: 10px 15px;
-	        margin-bottom: 15px;
-	        -webkit-border-radius: 10px;
-	    	-moz-border-radius: 10px;
-	    	border-radius: 10px;
-	    }
 
-	ul {
-		padding-left:30px;
-	}
+			<div id="sidebar" class="widget-area" role="complementary">
+				<ul class="xoxo">
+					<li id="text-3" class="widget-container widget_text"><h3 class="widget-title">Beta</h3>
+							<div class="widget-content">
+								<p>This is an early prototype and is a work-in-progress. </p>
+								<p>Please drop comments/questions/bug reports/feedback to <a href="mailto:aidinfo@practicalparticipation.co.uk">aidinfo@practicalparticipation.co.uk</a> or discuss on the <a href="http://aidinfolabs.org/archives/422">AidInfoLabs site</a>.
+								</p>
+							</div>
+						</li>	
 
-	.tool {
-		display:block;
-		clear:all;
-		margin:10px;
-	}
-	.note {
-		display:block;
-		clear:left;
-		font-size:smaller;
-		margin-top:10px;
-	}
-	.tool_link {
-		display:block;
-		float:right;
-	}
-
-	label {
-		font-weight:bold;
-	}
-
-	.loading {
-		background-image:url('loading.gif');
-		background-repeat:no-repeat;
-		background-position: 370px 0px;
-	}
-
-	#package_list {
-		width:400px;
-	}
-
-	fieldset {
-		margin-bottom:10px;
-		padding:5px;
-	}
-
-	.footnote {
-		font-size:small;
-	}
---></style>
-
-	<script><!--
-		$(document).ready(function() {
-			function ckan_populate(search,offset) {
-				limit = 20;
-				if(search) { search_string = "q="+search; } else { search_string = "res_format=IATI-XML"; }
-				if(offset == null) { offset = 0; }
-				$.ajax({
-				  url: "http://iatiregistry.org/api/1/search/package?"+search_string+"&all_fields=1&limit="+limit+"&offset="+offset,
-				  context: document.body,
-				  success: function(data){
-					if(search == $("#ckan_search").val()) { //In case the search has changed in the meantime
-						count = data['count'];
-						$("option.message").remove();
-						if(offset == 0) { $("#package_list").empty();
-										  $("#package_list").addClass("loading"); }
-						if(count == 0) { $("#package_list").append("<option value='' class='message'>No packages found...</option>"); }
-						$.each(data['results'], function(index, data) {
-							$("#package_list").append("<option value='"+data['name']+"'>"+data['title']+"</option>");
-						});
-						if((offset+limit)<count) {
-							if(offset == 0) {
-								$("#package_list").append("<option value='loadmore-"+limit+"' class='message'>Load "+(count-limit)+" more results...</option>");
-								$("#package_list").removeClass("loading");
-							} else {
-								ckan_populate(search,offset+limit);
-							}
-						} else {
-							$("#package_list").removeClass("loading");
-						}
-					}
-				  }
-				});
-			}
-
-			$("#package_list").change(function(data) {
-				if($(this).val().indexOf("loadmore") !==-1)
-				 {
-					$("#package_list").addClass("loading");
-					ckan_populate($("#ckan_search").val(),parseInt($(this).val().replace("loadmore-","")));
-				} else {
-					$(".url_field").val('');
-					$("#xml").val('Updating...');
-						$.ajax({
-						  url: "http://iatiregistry.org/api/1/rest/package/"+$(this).val(),
-						  context: document.body,
-						  success: function(data){
-							$(".url_field").val(data['download_url']);
-							$(".id_field").val(data['name']);
-						  }
-					    });
-				}
-			});
-
-			$("legend, #iati_reg_expand ").click(function() {
-				$(this).parents("fieldset").children("div").toggle('slow');
-			});
-
-			$("#download, #reset_link").click(function() {
-				$(".section").toggle('fast');
-			});
-
-			$("#xml").change(function(){
-				$(".id_field").val("Custom");
-			});
-
-			$("#search_index").click(function(){
-				$("#package_list").empty();
-				$("#package_list").append("<option value=''>Loading...</option>");
-				ckan_populate($("#ckan_search").val())
-			});
-
-			ckan_populate('');
-		});
-
-	--></script>
-	</head>
-	<body>
-
-	<form method="get" action=".">
-		<h1 class="title">CSV Conversion Tool</h1>
-			<div class="section">
-				<h3>(1) Select the data to convert</h3>
-
-				<fieldset>
-				    <legend>IATI Registry Search:</legend>
-					<div class="ckan" <?php if($_GET['xml']) { echo "style='display:none;'"; }?>>
-						<label for="ckan_search">Search: <input type="text" name="search" class="ckan_search" id="ckan_search"/></label> <input type="button" value="Search" id="search_index"><br/><br/>
-						<select name="package" id="package_list" size="10">
-							<option value="">Loading...</option>
-						</select><br/>
-						<span class="instruction">Select an item from the list above, or narrow the selection by typing in a search query (e.g. a country or donor name).</span>
+						<li id="text-3" class="widget-container widget_text"><h3 class="widget-title">About the Data</h3>					
+						<div class="widget-content">
+							<p>
+							Data is published part of the International Aid Transparency Initiative by individual donors, often in a different file for each country they work in, and is listed in the <a href="http://iatiregistry.org/">IATI Registry</a>. This tool uses data fetched from all those files, and aggregated together in an <a href="http://www.exist-db.org">XML database</a> to allow you to query the data and convert it into the format you want.
+							</p>
+							
+							
+						</div>
+					</li>
+					
+					<li id="text-3" class="widget-container widget_text"><h3 class="widget-title">Useful links</h3>					
+					<div class="widget-content">
+						<p><a href="http://www.iatiregistry.org" target="_blank">IATI Registry</a> (IATI Data)</p>
+						<p><a href="http://www.aidtransparency.net" target="_blank">Aid Tranparency </a> (IATI Project Site)</p>
+						<p><a href="http://www.aidinfolabs.org" target="_blank">Aid Info Labs</a> (Ideas and tools)</p>
+						<p><a href="http://www.github.com/aidinfolabs" target="_blank">Aid Info Labs on GitHub</a> (Shared source code)</p>
+						<p><a href="http://www.aidinfo.org" target="_blank">AidInfo</a> (Aid information news and update)</p>
+						</p>
+						
+						
 					</div>
-					<div class="search" <?php if(!$_GET['xml']) { echo "style='display:none;'"; }?>>
-						<span class="instruction">Enter/confirm the URL below, or <A href="#" id="iati_reg_expand">select a file from the IATI registry</a>.</span>
-					</div>
-				</fieldset>
-
-				<fieldset>
-				    <legend>Raw data address:</legend>
-					<label for="xml">URL: <input class="url_field" type="text" size="60" value="<?php echo $_GET['xml']; ?>" id="xml" name="xml" /></label>
-				</fieldset>
-			</div>
-
-			<div class="section">
-				<h3>(2) Select File Format</h3>
-				Select the CSV (spreadsheet) serialisation of IATI data you would prefer.
-					<input type="hidden" name="download" value="true" />
-					<input type="hidden" name="id" value="true" class="id_field"/>
-				<?php
-					if($_GET['format']) { $checked = $_GET['format']; } else {$checked = array_shift(array_keys($transformations)); }
-					foreach($transformations as $key => $transdata) {
-					echo "<div class='format'>";
-					echo "<label for='{$key}'><input type='radio' name='format' value='{$key}' id='{$key}'".($key == $checked ? "checked='checked'" : "") ."/> {$transdata['title']} (CSV)</label><br/>";
-					echo "<span class='description'>".$transdata['description']."</span>";
-					echo "</div>";
-				}?>
-				<input type="Submit" value="Download" id="download"/ >
-
-			</div>
-	</form>
-			<div class="section" style="display:none;">
-				<h3><a name="use">(3) Use your data</a></h3>
-				<br/>
-				<p>
-				Your file should start downloading shortly. For very large files this process could take a few minutes*.
-				</p>
-				<p>
-				If you would like to download another file once this is complete, <a href="#" id="reset_link">click here</a>.
-				</p>
-				<h4>Once you have IATI data in CSV format</h4>
-				<p>
-				You can:
-				<ul>
-					<li>Open it in Excel or other spreadsheet software;
-					<li>Import the data into your preferred statistical software package;
-					<li><a href="http://www.aidinfolabs.org/archives/444" target="_blank">Reshape the data using Google Refine</a>;
-					<li>And lots more
+				</li>
+				
 				</ul>
-				</p>
-				<p>
-				You can <a href="http://support.iatistandard.org/entries/20680393-csv-spreadsheet-version-of-iati" target="_blank">discuss use of IATI in CSV format on the IATI Support Knowledge Base website</a>.
-				</p>
-				<h4>Troubleshooting</h4>
-				<p>
-				<i>Having difficulty opening your downloaded file?</i> Check that it has a .csv file extension and that Excel, or your spreadsheet software, is configured as the default application for this kind of file. If clicking the file will not open it, try accessing it from the File->Open menu of your spreadsheet software.
-				</p>
-				<p>
-				<i>Need a different format?</i> Turning the rich raw IATI data files into a flat spreadsheet form means we have make a decision about what to include in the spreadsheet version. Sometimes the default choices available here might not suit your proposed use of IATI data. Get in touch via <a href="http://support.iatistandard.org" target="_blank">IATI Support</a> if you are in need of a different data format .
-				</p>
-				<span class="footnote">*There are a few IATI XML files that are too large to convert with this tool. For help with these please contact IATI support explaining how you plan to use them for advice on the best approaches to convert this data.</span>
-			</div>
-	<hr/>
-	<small>Developed by <a href="http://www.practicalparticipation.co.uk" target="_blank">Practical Participation</a> for <a href="http://www.aidinfolabs.org" target="_blank">aidinfo labs</a> using <a href="https://github.com/aidinfolabs/IATI-XSLT/" target="_blank">XSLT</a> based on work by Rob McKinnon. It works with valid activity data files prepared using the <a href="http://www.iatistandard.org" target="_blank">International Aid Transparency Initiative standard</a>. <a href="https://www.aidtransparency.net/privacy-policy">Privacy Policy</a></small>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110230511-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-110230511-2', {'anonymize_ip': true});
-</script>
-	</body>
+			</div><!-- #sidebar .widget-area -->
+		</div><!-- #main -->
+
+				<div id="footer" role="contentinfo"> 
+					<div id="colophon"> 
+ 						<div id="site-generator" class="clearfix"> 
+						</div><!-- #site-generator --> 
+					</div><!-- #colophon --> 
+				</div><!-- #footer --> 
+
+			</div><!-- #wrapper -->
+	
+	
+
+
+
+
+</body>
 </html>
